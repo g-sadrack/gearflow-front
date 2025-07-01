739439b8a770f76070333733184d6a6be7420ca8
sadrack <gabrielscr2023@gmail.com>
2025-07-01 17:18:11 -0300

    feat: adicionada as rotas para o navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { ClienteCadastro } from './features/cliente/cliente-cadastro/cliente-cadastro';
+import { ClienteBusca } from './features/cliente/cliente-busca/cliente-busca';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'cadastro-cliente', component: ClienteCadastro },
+  { path: 'busca-cliente', component: ClienteBusca },
+];
