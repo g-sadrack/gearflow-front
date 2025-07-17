fbbc784807a13bebd495dabdd324ab4fda6d973e
sadrack <gabrielscr2023@gmail.com>
2025-07-16 21:29:23 -0300

    feat: implementada a pagina de listagem de clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { ClienteCadastro } from './features/cliente/cliente-cadastro/cliente-cadastro';
+import { ClienteListagem } from './features/cliente/cliente-listagem/cliente-listagem';
 
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'cadastro-cliente', component: ClienteCadastro },
+  { path: 'listagem-cliente', component: ClienteListagem },
 ];
